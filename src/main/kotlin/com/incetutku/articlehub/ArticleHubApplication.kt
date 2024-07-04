e299b5528cdd57de6541a0431948cf5bbd785247
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-04 23:51:44 +0300

    initial commit

---
+++ src/main/kotlin/com/incetutku/articlehub/ArticleHubApplication.kt
@@ -0,0 +1,11 @@
+package com.incetutku.articlehub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArticleHubApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArticleHubApplication>(*args)
+}
---
+++ src/test/kotlin/com/incetutku/articlehub/ArticleHubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.incetutku.articlehub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArticleHubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
