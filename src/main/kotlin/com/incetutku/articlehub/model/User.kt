a15846b9ab1a8d39ce5f64dc21dce2fe040b1387
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-15 23:26:04 +0300

    user data

---
+++ src/main/kotlin/com/incetutku/articlehub/model/User.kt
@@ -0,0 +1,14 @@
+package com.incetutku.articlehub.model
+
+import java.util.UUID
+
+data class User(
+    val id: UUID,
+    val email: String,
+    val password: String,
+    val role: Role
+)
+
+enum class Role {
+    USER, ADMIN
+}
