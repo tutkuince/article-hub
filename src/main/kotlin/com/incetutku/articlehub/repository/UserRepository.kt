ac9d5fba59987e5be12abe41a553d2759483ab4e
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-15 23:26:27 +0300

    User Repository

---
+++ src/main/kotlin/com/incetutku/articlehub/repository/UserRepository.kt
@@ -0,0 +1,41 @@
+package com.incetutku.articlehub.repository
+
+import com.incetutku.articlehub.model.Role
+import com.incetutku.articlehub.model.User
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+class UserRepository {
+
+    // Dummy DB
+    private val users = mutableListOf(
+        User(
+            id = UUID.randomUUID(),
+            email = "email-1@gmail.com",
+            password = "pass1",
+            role = Role.USER
+        ),
+        User(
+            id = UUID.randomUUID(),
+            email = "email-2@gmail.com",
+            password = "pass2",
+            role = Role.ADMIN
+        ),
+        User(
+            id = UUID.randomUUID(),
+            email = "email-3@gmail.com",
+            password = "pass3",
+            role = Role.USER
+        )
+    )
+
+    fun save(user: User): Boolean = users.add(user)
+    fun findAll(): List<User> = users
+    fun findByEmail(email: String): User? = users.firstOrNull { it.email == email }
+    fun findByUUID(uuid: UUID): User? = users.firstOrNull { it.id == uuid }
+    fun deleteByUUID(uuid: UUID): Boolean {
+        val foundUser = findByUUID(uuid)
+        return foundUser?.let { users.remove(it) } ?: false
+    }
+}
