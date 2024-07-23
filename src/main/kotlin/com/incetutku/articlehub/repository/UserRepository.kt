31f79346bb423d28c7a1968a2401b8f275728268
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-24 01:06:31 +0300

    Update UserRepository for password encode

--- src/main/kotlin/com/incetutku/articlehub/repository/UserRepository.kt
+++ src/main/kotlin/com/incetutku/articlehub/repository/UserRepository.kt
@@ -2,35 +2,40 @@ package com.incetutku.articlehub.repository
 
 import com.incetutku.articlehub.model.Role
 import com.incetutku.articlehub.model.User
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-class UserRepository {
+class UserRepository(private val encoder: PasswordEncoder) {
 
     // Dummy DB
     private val users = mutableListOf(
         User(
             id = UUID.randomUUID(),
             email = "email-1@gmail.com",
-            password = "pass1",
+            password = encoder.encode("pass1"),
             role = Role.USER
         ),
         User(
             id = UUID.randomUUID(),
             email = "email-2@gmail.com",
-            password = "pass2",
+            password = encoder.encode("pass2"),
             role = Role.ADMIN
         ),
         User(
             id = UUID.randomUUID(),
             email = "email-3@gmail.com",
-            password = "pass3",
+            password = encoder.encode("pass3"),
             role = Role.USER
         )
     )
 
-    fun save(user: User): Boolean = users.add(user)
+    fun save(user: User): Boolean {
+        val updated = user.copy(password = encoder.encode(user.password))
+        return users.add(updated)
+    }
+
     fun findAll(): List<User> = users
     fun findByEmail(email: String): User? = users.firstOrNull { it.email == email }
     fun findByUUID(uuid: UUID): User? = users.firstOrNull { it.id == uuid }
