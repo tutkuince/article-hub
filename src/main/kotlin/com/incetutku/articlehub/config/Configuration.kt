5b08145f150c8032970e01305e3dc68c63b90ca5
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-24 01:03:24 +0300

    Update Config

--- src/main/kotlin/com/incetutku/articlehub/config/Configuration.kt
+++ src/main/kotlin/com/incetutku/articlehub/config/Configuration.kt
@@ -1,9 +1,38 @@
 package com.incetutku.articlehub.config
 
+import com.incetutku.articlehub.repository.UserRepository
+import com.incetutku.articlehub.service.CustomUserDetailService
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableConfigurationProperties(JWTProperties::class)
 class Configuration {
+
+    @Bean
+    fun userDetailsService(userRepository: UserRepository): UserDetailsService =
+        CustomUserDetailService(userRepository)
+
+    @Bean
+    fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun authenticationProvider(userRepository: UserRepository): AuthenticationProvider =
+        DaoAuthenticationProvider()
+            .also {
+                it.setUserDetailsService(userDetailsService(userRepository))
+                it.setPasswordEncoder(encoder())
+            }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager =
+        config.authenticationManager
 }
